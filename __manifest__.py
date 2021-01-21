ece197d6a137779765aa4e1d21e489191478425e
Andres Gaviria T <44479766+agavariat@users.noreply.github.com>
2021-01-21 17:39:16 -0500

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2015 Antiun Ingeniería, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "VAT in leads",
-    "summary": "Add VAT field to leads",
+    "name": "form in leads",
+    "summary": "Add forms to leads",
     "version": "12.0.1.0.0",
     "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/crm",
-    "author": "Antiun Ingeniería S.L., Odoo Community Association (OCA)",
+    "author": "intresco,Andres Gaviria",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
